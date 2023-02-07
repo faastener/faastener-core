d7da17c5d35b6b10145c074c7215b615f9eb4370
Vladimir Yussupov <v.yussupov@gmail.com>
2023-02-07 01:22:31 +0100

    Initialize project structure

---
+++ src/main/java/org/faastener/core/FaastenerCoreApplication.java
@@ -0,0 +1,13 @@
+package org.faastener.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaastenerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaastenerCoreApplication.class, args);
+	}
+
+}
