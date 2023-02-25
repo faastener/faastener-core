47d94e4348c655239d99bd35629ec7e006018c28
Vladimir Yussupov <v.yussupov@gmail.com>
2023-02-25 23:32:31 +0100

    Ignore case for FilterType enum deserialization

--- src/main/java/org/faastener/core/model/FilterType.java
+++ src/main/java/org/faastener/core/model/FilterType.java
@@ -1,5 +1,10 @@
 package org.faastener.core.model;
 
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum FilterType {
     CONTAINS_ONE("containsOne"),
     CONTAINS_ANY("containsAny"),
@@ -11,5 +16,25 @@ public enum FilterType {
 
     private FilterType(String filterType) {
         this.filterType = filterType;
+    }
+
+    @JsonCreator
+    public static FilterType fromString(String filterType) {
+        if (Objects.isNull(filterType)) return null;
+        if ("containsOne".equalsIgnoreCase(filterType)) {
+            return FilterType.CONTAINS_ONE;
+        } else if ("containsAny".equalsIgnoreCase(filterType)) {
+            return FilterType.CONTAINS_ANY;
+        } else if ("lte".equalsIgnoreCase(filterType)) {
+            return FilterType.LTE;
+        } else if ("gte".equalsIgnoreCase(filterType)) {
+            return FilterType.GTE;
+        } else
+            return FilterType.BOOL;
+    }
+
+    @JsonValue
+    public String getFilterType() {
+        return filterType;
     }
 }
